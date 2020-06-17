a984d36a8ac054eb969e42001e01ef8ad81c057f
54665 <45261464+54665@users.noreply.github.com>
2020-06-17 21:30:11 +0800

    更新 unaws.sh

--- unaws.sh
+++ unaws.sh
@@ -9,6 +9,7 @@ echo "正在取消开机自启"
     rm -rf /etc/rc.d/rc.local
     wget https://raw.githubusercontent.com/54665/awspro/master/rc.local -O /etc/rc.d/rc.local
 echo "正在删除脚本配置文件"
+    rm -rf /root/aws.sh
 	rm -rf /root/.awsll
 echo "正在删除定时任务"
     crontab -r
