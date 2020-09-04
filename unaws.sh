dceaf848a53f796cd7a025bd1ce4faccfd1be07f
54665 <45261464+54665@users.noreply.github.com>
2020-09-05 00:12:21 +0800

    更新 unaws.sh
    卸载更彻底

--- unaws.sh
+++ unaws.sh
@@ -11,6 +11,14 @@ echo "正在取消开机自启"
 echo "正在删除脚本配置文件"
     rm -rf /root/aws.sh
 	rm -rf /root/.awsll
+echo "正在卸载相关软件包"
+	yum remove wget -y
+	yum remove epel-release -y
+	yum remove wget vnstat -y
+	apt-get remove wget -y
+	apt-get remove epel-release -y
+	apt-get remove vnstat -y
+	service vnstat start
 echo "正在删除定时任务"
     crontab -r
 echo "开始重启"
